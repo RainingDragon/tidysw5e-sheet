c6cee01fdfb9f92a98de3bad28e76a5ef2feaafe
TJ <Cyr-@users.noreply.github.com>
2021-06-26 23:44:33 -0500

    Fixes for system customization

--- src/scripts/app/ammo-switch.js
+++ src/scripts/app/ammo-switch.js
@@ -1,11 +1,11 @@
-export const tidy5eAmmoSwitch = function (html, actor) {
+export const tidysw5eAmmoSwitch = function (html, actor) {
 
   html.find('.ammo').each(function () {
     const element = $(this);
     const itemId = element.attr('data-id');
     const actor = game.actors.find(x => x.items.some(b => b.id === itemId));
     const item = actor.items.get(itemId);
-    const equippedOnly = game.settings.get('tidy5e-sheet', 'ammoEquippedOnly');
+    const equippedOnly = game.settings.get('tidysw5e-sheet', 'ammoEquippedOnly');
     const ammoItems = actor.items.filter(x => x.data.data.consumableType === "ammo" && (!equippedOnly || x.data.data.equipped));
     const target = item.data.data.consume.target;
     const ammoItemStrings = ['<option value=""></option>']
